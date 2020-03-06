55d589441fbf39a7a424d8db0db0ac52a02a0aeb
Meredith Gallop <meredith@trtl.co.uk>
2020-03-06 17:05:43 +0000

    started ui integration tests

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,6 +16,20 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   });
 
+  // Do the number buttons update the display of the running total?
+  it('should update the display of the running total when the numbers are clicked', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('93')
+  })
 
+  // Do the arithmetical operations update the display with the result of the operation?
+  it('should update the display with the result of the operation when the arithmetical operations are clicked', function(){
+    
+  })
+  // Can multiple operations be chained together?
+  // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+  // What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you’d prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
 
 });
